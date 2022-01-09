e7de5dbfc07e18aafa1b719bd211565bc6446334
Yash Patel <71980446+patelyash9775@users.noreply.github.com>
2022-01-09 18:10:50 +0530

    set operation

---
+++ Set operation (Union,Intersection,difference).py
@@ -0,0 +1,14 @@
+E={0,2,4,6,8}
+N={1,2,3,4,5}
+
+#union
+print("Union of E and N : ",E|N)
+
+#intersection
+print("Intersection of E and N : ",E&N)
+
+#Difference
+print("Difference of E and N : ",E-N)
+
+#Symmetric difference
+print("Symmetric difference of E and N : ",E^N)
